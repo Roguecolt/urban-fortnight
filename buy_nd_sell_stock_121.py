5a69b03bbf120dd67b883d9eedba4f08246fe519
Aravind <115916656+Roguecolt@users.noreply.github.com>
2025-02-25 17:27:58 +0530

    Create buy_nd_sell_stock_121.py
    Leetcode question 121

---
+++ buy_nd_sell_stock_121.py
@@ -0,0 +1,10 @@
+def maxProfit(prices):
+        max_profit=0
+        min_price=float('inf')
+        for price in prices:
+            if price < min_price:
+                min_price= price
+            elif price - min_price > max_profit:
+                max_profit=price-min_price
+        
+        return max_profit
